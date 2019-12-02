944b7d1b5beb0704dece56dfcd6626d6fb0355b7
Sola <solaocodes@gmai.com>
2019-12-02 14:31:36 -0500

    Add login form validation

--- src/Login.js
+++ src/Login.js
@@ -4,8 +4,13 @@ class Login extends Component {
   render() {
     return (
       <form className="login text-center">
-        <input className="text-input" type="text" name="email" placeholder="Email"/>
-        <input className="text-input" type="text" name="password" placeholder="Password"/>
+        <input className="text-input" type="email" name="email" placeholder="Email" required/>
+        <input className="text-input"
+               type="password"
+               name="password"
+               placeholder="Password"
+               minlength="8"
+               required />
         <input className="submit" type="submit" value="Login" />
       </form>
     );
